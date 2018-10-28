d58a39d9f1f8a3f0a2fef6677eb7acbf2ec19d16
JackKuo <kuohfjack@gmail.com>
2018-10-28 22:10:34 +0800

    fix bug & staff search feature

--- line_bot/views.py
+++ line_bot/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
-from backend.models import Member
+from backend.models import Member, GPM
 from .models import Staff
 
 
@@ -17,7 +17,30 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 def is_staff(user_id):
     """檢查是不是工作人員（幹部），才能查詢特殊資料"""
 
-    return Staff.objects.exists(user_id=user_id)
+    return Staff.objects.filter(user_id=user_id).exists()
+
+
+def is_chinese(word):
+    """檢查是否每個字都是中文"""
+
+    return all(u'\u4e00' <= c <= u'\u9fff' for c in word)
+
+
+def ncnu_staff_contact(name):
+    """查詢教職員資料"""
+    import requests, json
+    r = requests.get("https://bot.moli.rocks/ncnu-staff-contact/" + name)
+    r = json.loads(r.text)
+    total = len(r) - 1
+    text = ''
+    for index, val in enumerate(r):
+        if index == 0:
+            continue
+        else:
+            for f_index, i in enumerate(r[0]):
+                text += i + '：' + val[f_index] + '\n'
+            text += '\n'
+    return total, text
 
 
 def is_sid(recv):
@@ -64,7 +87,13 @@ def callback(request):
                 # 都是整數就準備來檢查看看是不是學號
                 if recv.isdigit() and is_staff(user_id):
                     # 回傳學號資料
-                    is_sid(recv)
+                    resp = is_sid(recv)
+                elif is_chinese(recv):
+                    total, contact = ncnu_staff_contact(recv)
+                    if total > 0:
+                        resp = contact
+                    else:
+                        resp = recv
                 else:
                     resp = recv
                 line_bot_api.reply_message(
